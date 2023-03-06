9f98533b0706115331368abeb57a71aa41cd790a
Emilia Paz <emiliapaz@chromium.org>
2023-03-05 16:45:25 -0800

    Add gerrit-cls.py that prints the CL information of the last week

---
+++ gerrit-cls.py
@@ -0,0 +1,50 @@
+import json
+import logging
+import requests
+
+BASE_URL = "https://chromium-review.googlesource.com/"
+OWNER = "emiliapaz@chromium.org"
+AGE = "6days"
+GERRIT_MAGIC_JSON_PREFIX = ")]}'\n"
+
+def decode_response(response):
+  """ Returns a decoded JSON as a dict from a Gerrit API REST request. """
+
+  content = response.content.strip().decode(response.encoding)
+  if not content:
+    logging.error('Invalid http response')
+    return content
+
+  if content.startswith(GERRIT_MAGIC_JSON_PREFIX):
+    index = len(GERRIT_MAGIC_JSON_PREFIX)
+    content = content[index:]
+
+  try:
+    return json.loads(content)
+  except ValueError:
+    logging.error("Invalid json content: %s", content)
+
+
+def print_landed_cls():
+  """ Prints CLs landed in the last week in markdown format. """
+
+  URL = "{}changes/?q=is:merged+owner:{}+-age:{}".format(BASE_URL, OWNER, AGE)
+  CLs = decode_response(requests.get(URL))
+
+  for cl in CLs:
+    cl_markdown = 'Landed [cl](crrev.com/c/' + cl['submission_id'] + '): ' + cl['subject']
+    print(cl_markdown)
+
+
+def print_reviewed_cls():
+  """ Prints the number of CLs reviewed in the last week. """
+
+  URL = "{}changes/?q=reviewer:{}+-owner:{}+-age:{}".format(BASE_URL, OWNER, OWNER, AGE)
+  CLs = decode_response(requests.get(URL))
+
+  print('CLs reviewed: ' , len(CLs))
+
+
+if __name__ == "__main__":
+  print_landed_cls()
+  print_reviewed_cls();
